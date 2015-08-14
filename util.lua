76f3b5d9c72ab87b65798f89a908c0863a94977c
Andreas Prell <andreas.h.prell@gmail.com>
2015-08-14 23:50:12 +0200

    Remove slice and concat

--- util.lua
+++ util.lua
@@ -11,33 +11,6 @@ function util.map(t, fn)
 	return setmetatable(m, getmetatable(t))
 end
 
--- Returns a slice (copy) of an array or subarray
--- Shares metatable with original array
-function util.slice(t, i, j)
-	i = i or 1
-	j = j or #t
-	if j < 0 then j = j + #t + 1 end
-	if i < 1 or j > #t or i > j then
-		return setmetatable({}, getmetatable(t))
-	end
-	local slice = {}
-	for k = i, j do
-		table.insert(slice, t[k])
-	end
-	return setmetatable(slice, getmetatable(t))
-end
-
--- Concatenates the elements of a table to form a string
-function util.concat(t, sep)
-	sep = sep or " "
-	local s = ""
-	for i = 1, #t do
-		s = s .. _tostring(t[i])
-		if i < #t then s = s .. sep end
-	end
-	return s
-end
-
 -- Overrides tostring to print table contents
 function tostring(n)
 	if type(n) == "table"  then
@@ -45,7 +18,7 @@ function tostring(n)
 		if mt ~= nil and mt.__tostring ~= nil then
 			return mt.__tostring(n)
 		else
-			return "{" .. util.concat(util.map(n, tostring), ", ") .. "}"
+			return "{" .. table.concat(util.map(n, tostring), ", ") .. "}"
 		end
 	else
 		return _tostring(n)
