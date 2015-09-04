caa7daf1e04e6834b1c9ea0c357c79acc563c7c1
Andreas Prell <andreas.h.prell@gmail.com>
2015-09-04 22:31:29 +0200

    Add built-in function print

--- tiny_builtin.lua
+++ tiny_builtin.lua
@@ -16,6 +16,8 @@ for sym, val in pairs {
 	["<"]  = function (a, b) return a < b end,
 	[">"]  = function (a, b) return a > b end,
 
+	["print"] = print,
+
 } do Env.add(builtin, sym, val) end
 
 return builtin
