207ce967d745aeda1f2085f6eda8fe67588185c4
Andreas Prell <andreas.h.prell@gmail.com>
2020-08-13 17:57:25 +0200

    Add option to dump the AST

--- src/main.lua
+++ src/main.lua
@@ -3,14 +3,18 @@
 package.path = "src/?.lua;" .. package.path
 
 local core = require "core"
-local parse = core.parse
+local parse = core.parse()
 local eval = core.eval
 local repl = core.repl
 
 if #arg > 0 then
+	if arg[1]:lower() == "-dump-ast" then
+		table.remove(arg, 1)
+		eval = print
+	end
 	for i = 1, #arg do
 		local file = assert(io.open(arg[i]))
-		eval(parse():match(file:read("*all")))
+		eval(parse:match(file:read("*all")))
 		file:close()
 	end
 else
